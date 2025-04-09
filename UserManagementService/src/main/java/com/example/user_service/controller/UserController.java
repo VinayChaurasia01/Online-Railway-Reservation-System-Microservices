1dbd8c69a5f064d43ba5ddb8d68e0813c1460fbd
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-04-10 02:48:17 +0530

    Vinay Add . User management service for user register and login

---
+++ UserManagementService/src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.user_service.controller;
+
+
+import com.example.user_service.model.User;
+import com.example.user_service.service.JWTService;
+import com.example.user_service.service.UserService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+//@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(HttpServletRequest request) {
+        String token = userService.extractToken(request);
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String email = jwtService.extractUsername(token); // extracting email
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/update-profile/{email}")
+    public ResponseEntity<String> updateUserProfile(@PathVariable String email, @RequestBody User updatedUser) {
+        User user = userService.updateUser(email, updatedUser);
+        return ResponseEntity.ok("Updated successdully !");
+    }
+}
---
+++ UserManagementService/src/main/java/com/example/user_service/service/JWTService.java
@@ -0,0 +1,73 @@
+package com.example.user_service.service;
+
+import com.example.user_service.model.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, Role role) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
